646943f3ac866aef7581fcdbaf72a1d4d59dcbe8
Sameh Mohamed <smohamed1@lenovo.com>
2020-10-19 10:32:42 -0400

    organizing the acm module a lot more

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   version = "~> 2.0"
   region = "us-east-1"
-  alias      = "local_account_regional"
+  alias      = "local_account_us_east"
   assume_role {
     role_arn = var.deploy_role_arn
   }
